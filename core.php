b69be24727a314d199d9297bae396e21c20914e7
overflowerror <mail@overflowerror.com>
2023-11-24 21:06:53 +0100

    feat: Add basic persistence layer + migration framework

--- core.php
+++ core.php
@@ -7,9 +7,12 @@ define("MAINTENANCE_MODE", require(ROOT . "/maintenance.php"));
 if (MAINTENANCE_MODE) {
     require(ROOT . "/templates/maintenance.php");
 } else {
+    $connection = require_once(ROOT . "/persistence/connection.php");
+    (require(ROOT . "/persistence/migrate.php"))($connection);
+
     $router = require(ROOT . "/router/Router.php");
-
     (require(ROOT . "/controllers/routes.php"))($router);
-
-    $router->execute();
+    $router->execute([
+        "DB_CONNECTION" => $connection,
+    ]);
 }
