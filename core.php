6a6d0b641e064518bbdb5790e4377e111c57465c
overflowerror <mail@overflowerror.com>
2023-11-24 13:44:32 +0100

    chore: Add deployment pipeline

--- core.php
+++ core.php
@@ -5,7 +5,7 @@ const ROOT = __DIR__;
 define("MAINTENANCE_MODE", require(ROOT . "/maintenance.php"));
 
 if (MAINTENANCE_MODE) {
-    require(ROOT . "./templates/maintenance.php");
+    require(ROOT . "/templates/maintenance.php");
 } else {
     $router = require(ROOT . "/router/Router.php");
 
